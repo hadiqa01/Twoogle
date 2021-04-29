26a7a6a09e24c57bc706fcba5b1592e2d680ef23
K <x1489@MacBooX.local>
2021-04-29 16:25:33 -0400

    reorganize project

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template, request
-app = Flask(__name__)
-
-
 import pandas as pd
-# from transformers import pipeline
 from transformers import pipeline, DistilBertForSequenceClassification, DistilBertTokenizerFast
 import tweepy
 
+
+# Private credentials to be withdrawn at the end of Spring '21
+KEY = "WD9TVjUQFAf5m6XuWMJd7vErs" # API Key
+SECRET =  "OtKCZvwqqBDiBMqdgip2BCE4Z9N6JpK6yg6ylNDYYxMhun29mn" # API Secret Key
+TOKEN = "1384529505696559104-Cxr6keJipox3TCJbBVsIz91poKkyPT" # Access Token
+TOKEN_SECRET = "aIDnNeJBKgupUnUBl8V3Sz0DWWCCpAM9cNFtg8LBRlgga" # Access Token Secret
+
+# Sentiment classes
 LABELS = {'LABEL_0': 'NEGATIVE', 'LABEL_1': 'POSITIVE'}
 
-# # for tweet in tweets:
-# #     print(tweet)
-# df = pd.DataFrame(data=tweets, columns=['Tweet'])
-# # df.head(5)
-# # print("df=" + df)
-# # Iterate over `tweets` and pass each item to `model()` to obtain
-# # prediction, then write those predictions to a Pandas dataframe
-# model = pipeline('sentiment-analysis')
-# df['Sentiment'] = list(model(t)[0].get('label') for t in tweets)
-# df['Score'] = list(model(t_)[0].get('score') for t_ in tweets)
+# Initialize Twitter API client
+auth = tweepy.OAuthHandler(KEY, SECRET)
+auth.set_access_token(TOKEN, TOKEN_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
+# Initialize Flask app
+app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
@@ -27,54 +27,36 @@ LABELS = {'LABEL_0': 'NEGATIVE', 'LABEL_1': 'POSITIVE'}
 def index():
     return render_template("index.html")
 
-#background process happening without any refreshing
 @app.route('/getTweets')
 def getTweets():
-    print("Getting tweets now...")
-    # keyword = requests.get('keyword', args)
-    #default keyword if you hit search. 
-    keyword = request.args.get('keyword', default='coronavirus covid vaccine vaccination COVID-19')
-    print("keyword= " + keyword)
-    #our entry to tweets
-    api_key = "WD9TVjUQFAf5m6XuWMJd7vErs" # API Key
-    api_secret =  "OtKCZvwqqBDiBMqdgip2BCE4Z9N6JpK6yg6ylNDYYxMhun29mn" # API Secret Key
-    api_access_token = "1384529505696559104-Cxr6keJipox3TCJbBVsIz91poKkyPT" # Access Token
-    api_access_token_secret = "aIDnNeJBKgupUnUBl8V3Sz0DWWCCpAM9cNFtg8LBRlgga" # Access Token Secret
+    print("Getting tweets now ...")
 
-    auth = tweepy.OAuthHandler(api_key, api_secret)
-    auth.set_access_token(api_access_token, api_access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True)
-    query = keyword # "coronavirus covid vaccine vaccination COVID-19" # text from the search box
-    tweets_ = tweepy.Cursor(api.search, query, result_type='recent').items(20) #change the items number to decrease / increase the tweets. the bigger the number the longer it'll take to retreive.
+    # Default keyword if you hit search
+    keyword = request.args.get('keyword',
+                               default='coronavirus covid vaccine vaccination COVID-19')
+    
+    # Fetch the 20 most recent tweets matching the query. Change the argument
+    # in `items()` to decrease or increase the number of retrieved tweets.
+    # The larger the number the longer the retreival time.
+    query = keyword # text from the search box
+    tweets_ = tweepy.Cursor(api.search, query, result_type='recent').items(20)
     tweets = [tweet.text for tweet in tweets_]    
 
     print("Done..retrieving tweets from API based on the keyword=" + keyword)
-    # auth = tweepy.OAuthHandler(api_key, api_secret)
-    # auth.set_access_token(api_access_token, api_access_token_secret)
-    # api = tweepy.API(auth, wait_on_rate_limit=True)
-    # query = "coronavirus covid vaccine vaccination COVID-19" # text from the search box
-    # tweets_ = tweepy.Cursor(api.search, query, result_type='recent').items(50)
-    # tweets = [tweet.text for tweet in tweets_]
-
-    # for tweet in tweets:
-    #     print(tweet)
+ 
     df = pd.DataFrame(data=tweets, columns=['Tweet'])
-    
     print("Done..creating dataframe")
-    # print(df)
-    # df.head(5)
-    # print("df=" + df)
+
     # Iterate over `tweets` and pass each item to `model()` to obtain
     # prediction, then write those predictions to a Pandas dataframe
     model = pipeline('sentiment-analysis',
-            model=DistilBertForSequenceClassification.from_pretrained("checkpoint-9500"),
+            model=DistilBertForSequenceClassification.from_pretrained("model"),
             tokenizer=DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased'))
-    # model = pipeline('sentiment-analysis')
+
     df['Sentiment'] = list(LABELS[model(t)[0].get('label')] for t in tweets)
     df['Score'] = list(model(t_)[0].get('score') for t_ in tweets)
-    # df['Sentiment'] = list(model(t)[0].get('label') for t in tweets)
-    # df['Score'] = list(model(t_)[0].get('score') for t_ in tweets)
-    print("Done..sentiment-analysis")
+
+    print("Done ... sentiment-analysis")
     print(df)
     return render_template("covid.html", data=list(df.values.tolist()))   
 
